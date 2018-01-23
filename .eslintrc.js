3c4259fc7bb82c6718fa390a06ba5b62939f8bcb
ChaselWu <minowu@foxmail.com>
2018-01-23 15:01:13 +0800

    Init project

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    node: true
+  },
+  extends: 'standard',
+  // add your custom rules here
+  'rules': {
+    'no-useless-escape': 0,
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': 0
+  }
+}
