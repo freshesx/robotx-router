eebf302e0c9f0a93440c23e046b4e79ec251bfa5
ChaselWu <minowu@foxmail.com>
2018-01-24 16:33:53 +0800

    Update HIstory test file.

--- src/core/__test__/History.test.js
+++ src/core/__test__/History.test.js
@@ -2,7 +2,7 @@ import History from '../History'
 import { emptySessionStorage } from './mocks/sessionStorage'
 
 describe('History', () => {
-  test('#addItem with root item', () => {
+  test('#addItem: add a history item without parent', () => {
     const selfItem = {
       name: 'products'
     }
@@ -10,18 +10,18 @@ describe('History', () => {
     window.sessionStorage = emptySessionStorage
 
     const history = new History()
-    history.addItem(selfItem)
-    console.log(history.collection)
-    const item = history.collection[0]
+    const savedItem = history.addItem(selfItem)
 
-    expect(item.name).toEqual('products')
-    expect(item.id).toEqual(0)
-    expect(item.parent).toBeNull()
-    expect(item.child).toBeNull()
-    expect(item.queries).toEqual({})
+    expect(savedItem).toEqual({
+      id: 1,
+      name: 'products',
+      parent: null,
+      child: null,
+      queries: {}
+    })
   })
 
-  test('#addItem, selfItem hasn\'t name.', () => {
+  test('#addItem: add a history item without it\'s name', () => {
     const selfItem = {}
 
     window.sessionStorage = emptySessionStorage
@@ -31,5 +31,18 @@ describe('History', () => {
     expect(() => {
       history.addItem(selfItem)
     }).toThrowError()
+  })
+
+  test('#addItem: add a history item and then add it\'s child history item', () => {
+    // setup
+    window.sessionStorage = emptySessionStorage
+    const history = new History()
+    
+    const firstItem = { name: 'products' }
+    const secondItem = { name: 'cases' }
+
+    const firstSavedItem = history.addItem(firstItem)
+    const secondSavedItem = history.addItem(secondItem, firstItem)
+    expect(secondSavedItem.parent.id).toEqual(firstSavedItem.id)
   })
 })
