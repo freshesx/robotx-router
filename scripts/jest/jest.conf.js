90505524e9b702c40be61141d4962ef6eb4be569
ChaselWu <minowu@foxmail.com>
2018-01-25 12:24:04 +0800

    Add setup file.

--- scripts/jest/jest.conf.js
+++ scripts/jest/jest.conf.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = {
   rootDir: path.resolve(__dirname, '../../'),
+  setupFiles: [ path.resolve(__dirname, './setup.js') ],
   mapCoverage: true,
   collectCoverage: true,
   coverageDirectory: '<rootDir>/scripts/jest/coverage',
---
+++ scripts/jest/setup.js
@@ -0,0 +1,4 @@
+window.sessionStorage = {
+  getItem: () => null,
+  setItem: () => null
+}
