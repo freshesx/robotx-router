22937f8854ae90e347b40069e928377c98e270bd
ChaselWu <minowu@foxmail.com>
2018-01-25 23:06:47 +0800

    Update example

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -6,15 +6,15 @@ Vue.use(RxRouter)
 
 // 2. Define route components
 const Home = { template: '<div>home</div>' }
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
+const Foo = { template: '<div>products</div>' }
+const Bar = { template: '<div>cases</div>' }
 
 // 3. Create the router
 const rxRouter = new RxRouter({
   routes: [
-    { name: 'products', component: Home },
-    { name: 'caes', component: Foo },
-    { name: 'users', component: Bar }
+    { name: 'homepage', component: Home },
+    { name: 'products', component: Foo },
+    { name: 'cases', component: Bar }
   ]
 })
 
@@ -25,6 +25,24 @@ new Vue({
   rxRouter,
   template: `
     <div id="app">
+      <div class="navbar">
+        <button @click="pushTask('homepage')">Homepage</button>
+        <button @click="pushTask('products')">products</button>
+        <button @click="pushTask('cases')">cases</button>
+      </div>
+      <div class="tasks">
+        <div
+          class="task"
+          v-for="task in $rxTasks" :key="task.id"
+        >{{ task.id }}</div>
+        <div class="task task-plus" @click="addTask">+</div>
+      </div>
+      <div class="views">
+        <div
+          class="view"
+          v-for="task in $rxTasks" :key="task.id"
+        >{{ task.id }}</div>
+      </div>
       <div v-for="(collection, name) in collections">
         <h1>{{ name }}</h1>
         <ul>
@@ -42,6 +60,26 @@ new Vue({
         records: this.$rxRecords,
         tasks: this.$rxTasks
       }
+    }
+  },
+  methods: {
+    addTask () {
+      this.$rxRouter.addTask('homepage')
+    },
+    pushTask (name) {
+      this.$rxRouter.pushTask(name)
+    },
+    removeTask () {
+
+    },
+    updateRecord () {
+
+    },
+    prevRecord () {
+
+    },
+    nextRecord () {
+
     }
   },
   mounted () {
