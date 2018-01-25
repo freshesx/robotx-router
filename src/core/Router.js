e2053886571a8143e558d4298b7b0c12fd73f107
ChaselWu <minowu@foxmail.com>
2018-01-25 21:07:27 +0800

    Update Router file.

--- src/core/Router.js
+++ src/core/Router.js
@@ -1,41 +1,96 @@
 // @flow
-import History from './History'
+
+interface RecordItem {
+  id: number,
+  name: string,
+  prevId?: number,
+  nextId?: number,
+  query?: Object
+}
+
+interface TaskItem {
+  id: number,
+  recordId: number
+}
+
+interface componentItem {
+  name: string,
+  meta: ?Object,
+  component: Object|Function
+}
 
 export default class Router {
-  /**
-   * Store routes config
-   */
-  // routes: any
-  history: History
+  records:Array<RecordItem>
+  tasks:Array<TaskItem>
+  components:Array<componentItem>
+
+  recordId: number
 
   /**
    * Define router config.
    */
-  constructor () {
-    this.history = new History()
+  constructor (options) {
+    this.records = []
+    this.tasks = []
+    this.components = options.routes
+
+    // private variable
+    this.recordId = 1
   }
 
-  push (routeName:string, routeQueries:Object, taskId:number) {
+  addTask (recordName:string) {
+    const record:RecordItem = this.addRecord(recordName)
 
+    const task:TaskItem = {
+      id: this.newId(),
+      recordId: record.id
+    }
+
+    this.tasks.push(task)
   }
 
-  update (routeQueries:Object, historyId:number) {
-
+  newId ():number {
+    return new Date().getTime()
   }
 
-  toggle (taskId:number) {
-
+  newRecordId ():number {
+    return this.recordId++
   }
 
-  pushNew (routeName:string, routeQueries:Object) {
+  addRecord (name: string, params:{ query?: Object, prevId?: number } = {}):RecordItem {
+    if (!name) {
+      throw new Error('Could not find record item name.')
+    }
 
+    const item:RecordItem = {
+      id: this.newRecordId(),
+      name: name
+    }
+
+    // If has query
+    if (params.query) {
+      item.query = params.query
+    }
+
+    // If prevItem is exits, then set prevItem's next is current item
+    if (params.prevId) {
+      const prevItem:?RecordItem = this.findRecord(params.prevId)
+
+      if (!prevItem) {
+        throw new Error('Could not find the previous record item.')
+      }
+
+      prevItem.nextId = item.id
+      item.prevId = prevItem.id
+    }
+
+    this.records.push(item)
+
+    return item
   }
 
-  destroy (taskId:number, needActiveTaskId:number) {
-
-  }
-
-  move () {
-
+  findRecord (recordId: number):?RecordItem {
+    // Use reverse, because what you want to get is the latest value.
+    return this.records.reverse().find(item => item.id === recordId)
   }
 }
