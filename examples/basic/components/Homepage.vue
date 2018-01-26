d152828fc8618da9af4df82072a02e89d343b71c
ChaselWu <minowu@foxmail.com>
2018-01-26 20:40:50 +0800

    Update Homepage.

--- examples/basic/components/Homepage.vue
+++ examples/basic/components/Homepage.vue
@@ -2,5 +2,33 @@
   <div>
     <h1>Homepage</h1>
     <div>Description</div>
+    <div>Your name: {{ models.username }}</div>
+
+    <form @submit="submit">
+      <input type="text" v-model="models.username">
+      <button type="submit">Confirm</button>
+    </form>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      models: {
+        username: undefined
+      }
+    }
+  },
+  methods: {
+    submit (event) {
+      event.preventDefault()
+      console.log('hi', this.models)
+      this.$rxRouter.updateTask({
+        query: this.models
+      })
+      console.log('this', this)
+    }
+  }
+}
+</script>
