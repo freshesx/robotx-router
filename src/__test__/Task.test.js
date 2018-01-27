63299627d7c31f225780df65862c7dbe28cf8c3a
ChaselWu <minowu@foxmail.com>
2018-01-27 15:28:06 +0800

    Add Task test file.

---
+++ src/__test__/Task.test.js
@@ -0,0 +1,25 @@
+import Task from '../Task.js'
+import Record from '../Record.js'
+
+let page
+let record
+
+describe('Task', () => {
+  beforeEach(() => {
+    page = {
+      name: 'homepage',
+      component: {
+        template: 'Hi'
+      }
+    }
+    record = new Record(0, page)
+  })
+
+  it('returns a created task', () => {
+    const task = new Task(record)
+    expect(task).toEqual({
+      uid: 0,
+      record
+    })
+  })
+})
